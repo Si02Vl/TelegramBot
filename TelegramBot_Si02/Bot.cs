dcc7c183c1fcb25e141f8dc0e8bf39037a4bb201
PS <sinatel@inbox.ru>
2022-09-22 10:04:22 +1000

    Добавьте файлы проекта.

---
+++ TelegramBot_Si02/Bot.cs
@@ -0,0 +1,59 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+
+namespace Bot
+{
+    class Bot
+    {
+        static ITelegramBotClient bot = new TelegramBotClient("5526483933:AAGPLXbHWd_AkqUOaVkRL9qmJ-zdwXVIobA");
+
+
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            var message = update.Message;
+            // Некоторые действия
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                if (message.Text.ToLower() == "/start")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, "Введена команда START");
+                    return;
+                }
+                else if (message.Text.ToLower() == "/end")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, "Введена команда END");
+                    return;
+                }
+
+                await botClient.SendTextMessageAsync(message.Chat, $"Дата сообщения {message.Date}");
+            }
+        }
+
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            // Некоторые действия
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
+
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { }, // receive all update types
+            };
+            bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+            Console.ReadLine();
+        }
+    }
+}
