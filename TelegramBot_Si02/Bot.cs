8c3f2ba835e7e8ac5a68e117d7d703cdc75901aa
Si02Vl <sinatel@inbox.ru>
2024-01-09 16:14:21 +1000

    09.01.24

--- TelegramBot_Si02/Bot.cs
+++ TelegramBot_Si02/Bot.cs
@@ -8,6 +8,21 @@ namespace Bot
     {
         static ITelegramBotClient bot = new TelegramBotClient("5396048539:AAEugLYh52N30Khic5HilGXyQvFDXXvyn94");
         
+        public async Task SendMessageToAllUsers(string botToken, string messageText)
+        {
+            var botClient = new TelegramBotClient("5396048539:AAEugLYh52N30Khic5HilGXyQvFDXXvyn94");
+
+            // Получаем список всех пользователей бота
+            var botUpdates = await botClient.GetUpdatesAsync();
+            var users = botUpdates.Select(u => u.Message.Chat).Distinct();
+
+            // Отправляем сообщение каждому пользователю
+            foreach (var user in users)
+            {
+                await botClient.SendTextMessageAsync(user.Id, "Дырявые носки");
+            }
+        }
+        
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
             var message = update.Message;
@@ -27,7 +42,7 @@ namespace Bot
                 }
 
                 await botClient.SendTextMessageAsync(message.Chat, $"Дата сообщения {message.Date}, " +
-                                                                   $"Пользователь {message.From}, " +
+                                                                   $"Пользователь {message.Chat.FirstName}, " +
                                                                    $"Текст сообщения {message.Text}");
             }
         }
@@ -51,6 +66,7 @@ namespace Bot
             bot.StartReceiving(
                 HandleUpdateAsync,
                 HandleErrorAsync,
+                SendMessageToAllUsers,
                 receiverOptions,
                 cancellationToken
             );
