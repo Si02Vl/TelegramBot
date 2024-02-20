5a7e092d18942109451be9d524bfa0355b6a5a62
Si02Vl <sinatel@inbox.ru>
2024-02-20 14:14:54 +1000

    20.02.24

--- TelegramBot/InlineKeyboardHandler.cs
+++ TelegramBot/InlineKeyboardHandler.cs
@@ -20,27 +20,29 @@ namespace TelegramBot
         public static async Task InlineKeyboardActionAsync(CallbackQuery callbackQuery, ITelegramBotClient botClient, long chatId)
         {
             var filePath = "C:/Users/user/RiderProjects/TelegramBot_Si02/TelegramBot/shoppingListData.txt";
-            var file = await File.ReadAllTextAsync(filePath); 
+            var items = await File.ReadAllLinesAsync(filePath); 
+            
             var button = InlineKeyboardDataGetting(callbackQuery); 
             var clearButtonData = Regex.Replace(button, "button_|_data", "");
 
-            var lines = file.Split(Environment.NewLine);
-    
-            for (int i = 0; i < lines.Length; i++)
+            bool found = false;
+
+            for (int i = 0; i < items.Length; i++)
             {
-                if (lines[i].Contains(clearButtonData))
+                if (items[i] == clearButtonData)
                 {
-                    // добавляем зачеркивание к строке
-                    lines[i] = $"<s>{lines[i]}</s>";
                     // отправляем сообщение
                     string messageToChat = "Нажатие: " + clearButtonData;
                     await botClient.SendTextMessageAsync(chatId, messageToChat, parseMode: Telegram.Bot.Types.Enums.ParseMode.Html);
+                    // добавляем зачеркивание только к совпавшей строке
+                    items[i] = $"<s>{items[i]}</s>"; 
+                    //found = true;
+                    var updatedFileContent = string.Join(Environment.NewLine, items);
+                    await File.WriteAllTextAsync(filePath, updatedFileContent);
                     break;
                 }
-            }
 
-            var updatedFileContent = string.Join(Environment.NewLine, lines);
-            await File.WriteAllTextAsync(filePath, updatedFileContent);
+            }
         }
     }
 }
